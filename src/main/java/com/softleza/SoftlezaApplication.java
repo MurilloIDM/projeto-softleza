92d48a033666986d230db04e5be7cafe6ff70452
MurilloIDM <murilloidm05@gmail.com>
2020-10-12 07:53:29 -0300

    New: Inicia projeto

---
+++ src/main/java/com/softleza/SoftlezaApplication.java
@@ -0,0 +1,13 @@
+package com.softleza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftlezaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftlezaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softleza/SoftlezaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softleza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftlezaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
